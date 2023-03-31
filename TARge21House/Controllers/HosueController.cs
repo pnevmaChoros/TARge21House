3e1aa6861dc520bd47dbd44f08d6e9224faaa3f9
daniel dogadajev <daniel.dogadajev@gmail.com>
2023-03-31 19:35:34 +0300

    Created HouseController

--- TARge21House/Controllers/HosueController.cs
+++ TARge21House/Controllers/HosueController.cs
@@ -1,6 +1,40 @@
-﻿namespace TARge21House.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TARge21House.Data;
+using TARge21House.Models.House;
+
+namespace TARge21House.Controllers
 {
-	public class HosueController
+	public class HosueController : Controller
 	{
+		private readonly TARge21HouseContext _context;
+
+        public HosueController
+			(
+				TARge21HouseContext context
+			)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+		{
+			var result = _context.Houses
+				.OrderByDescending(h => h.CreatedAt)
+				.Select(x => new HouseIndexViewModel
+				{
+					Id = x.Id,
+					Address = x.Address,
+					City = x.City,
+
+					Floors = x.Floors,
+					Area = x.Area,
+					Price = x.Price,
+
+					CreatedAt = x.CreatedAt,
+					ModifiedAt = x.ModifiedAt
+				});
+
+			return View(result);
+		}
 	}
 }
